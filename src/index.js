0e7b97fa54c89bb96b46cb4d8efa138d621ef346
Hunter Baker <hbake@tamu.edu>
2022-01-18 15:05:23 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var ejs = require('ejs');
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('App listening at http://%s:%s', host, port);
+});
+
+app.set('view engine', 'ejs');
+
+var pages = [
+    {name: 'Index', link: '/', file: 'index.ejs'},
+    {name: 'Portfolio', link: 'portfolio', file: 'portfolio.ejs'},
+    {name: 'Qualifications', link: 'qualifications', file: 'qualifications.ejs'},
+]
+
+pages.forEach( (page) => {
+    app.get('/' +page.link, (req, res) => {
+        res.render(__dirname + '/pages/' + page.file, {
+            currentPage: page.link,
+            pages: pages
+        });
+    })
+})
+
+app.use(express.static(__dirname + '/public'));
